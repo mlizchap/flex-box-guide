890d2e273e0e26ef1998f61a0db9b9994681030e
mlizchap <mlizchap@gmail.com>
2018-11-05 23:21:34 -0500

    styled flex-grow display

--- src/components/ChildPropsDropDown.js
+++ src/components/ChildPropsDropDown.js
@@ -6,35 +6,44 @@ class ChildPropsDropDown extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            contentDisplay: "none",
-            currentNumber: 1
+            currentProp: "flexGrow",
+            display: {
+                flexGrow: "none",
+                flexShrink: "none"
+            },
+            currentNumber: {
+                flexGrow: 1,
+                flexShrink: 1
+            }
          };
     }
     showContent = () => {
-        this.props.setCurrent()
-        this.setState({ contentDisplay: "block" })
+        this.setState({ display: {...this.state.display, [this.state.currentProp]: "block" }}, () => console.log(this.state.display))
     }
     hideContent = () => {
-        this.setState({ contentDisplay: "none" })
+        this.setState({ display: {...this.state.display, [this.state.currentProp]: "none" }})
     }
-    setCurrentNumber = (e) => {
-        
-        this.setState({ 
-            currentNumber: e.target.innerHTML}, 
-            () => this.props.handleSelectNumber(this.props.letter, this.state.currentNumber))
+    setNumber = (e) => {
+        this.setState({ currentNumber: {...this.state.currentNumber, [this.state.currentProp]: e.target.innerHTML}})
     }
+
     render() {
         return (
-            <DropDownStyle showContent={this.state.contentDisplay} bgColor={this.props.bgColor}>
+            <DropDownStyle bgColor={this.props.bgColor}>
                 <h3>{this.props.letter}</h3>
-                <div onMouseEnter={this.showContent} onMouseLeave={this.hideContent}>
-                    <button>
-                        {this.state.currentNumber} <span className="arrow">&#9660;</span>
-                    </button>
-                    <div className="dropDownContent">
-                        { Array.from({length:5}, (_, i) => <div onClick={this.setCurrentNumber} key={i}>{i + 1}</div>) }
+
+                        <div>
+                            flex-grow:
+                            <div className="dropDownContent" onMouseEnter={() => this.showContent()} onMouseLeave={() => this.hideContent()}>
+                                <button>
+                                    {this.state.currentNumber.flexGrow} <span className="arrow">&#9660;</span>
+                                </button>
+
+                                <ContentDisplay showContent={this.state.display.flexGrow} >
+                                    { Array.from({length:5}, (_, i) => <div onClick={this.setNumber} key={i}>{i + 1}</div>) }
+                                </ContentDisplay>
+                            </div>
                     </div>
-                </div>
             </DropDownStyle>
         );
     }
@@ -45,27 +54,34 @@ export default ChildPropsDropDown;
 const DropDownStyle = styled.div`
     background-color: ${props => props.bgColor};
     padding: 10px;
-    width: 50px;
+    width: 200px;
     text-align: center;
     margin-bottom: 20px;
     display: inline-block;
     margin: 20px;
     border-radius: 2px;
     button {
+        display: inline-block;
         width: 50px;
         background-color: ${globalStyle.darkgray};
         color: ${props => props.bgColor};
     }
-    .dropDownContent {
-        display: ${props => props.showContent}
-        position: absolute;
-        background-color: ${globalStyle.lightgray};
-        width: 50px;
-        margin-right: auto;
-        margin-left: auto;
-        z-index: 3;
-    }
     .arrow {
         float: right
     }
+    .dropDownContent {
+        display: inline-block;
+    }
+`
+
+const ContentDisplay = styled.div`
+    display: ${props => props.showContent}
+    position: absolute;
+    z-index: 4;
+    background-color: ${globalStyle.lightgray};
+    width: 50px;
+    margin-right: 90px;
+    //display: block;
+
+    
 `
