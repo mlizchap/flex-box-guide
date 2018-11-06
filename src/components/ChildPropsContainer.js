3099569affaed8fd48dc896849b1eeb8795859dd
mlizchap <mlizchap@gmail.com>
2018-11-06 00:42:01 -0500

    removed extra item properties, combined into one display

--- src/components/ChildPropsContainer.js
+++ src/components/ChildPropsContainer.js
@@ -5,54 +5,78 @@ import { globalStyle } from '../globalStyle';
 import ChildPropsItemContainer from './ChildPropsItemContainer';
 import { flexBoxData } from '../static/flexBoxData';
 
+const items = ["a", "b", "c"]
+
 class ChildPropsContainer extends Component {
     constructor(props) {
         super(props);
         this.state = { 
+            currentProp: "",
             flexGrow: { a: 1, b: 1, c: 1},
             flexShrink: { a: 1, b: 1, c: 1},
         };
     }
     updateNumberSelection = (numberSelected, currentProperty, letter) => {
-        //console.log("SHR", this.state.flexShrink)
+        console.log(currentProperty)
             this.setState(
                 { [currentProperty]: {...this.state[`${currentProperty}`], [`${letter}`]: Number(numberSelected)} },
-                () => console.log(this.state.flexShrink)
-            )        
+                () => console.log(this.state)
+            )
     }
     render() {
         return (
             <div style={{marginBottom: '200px'}}>
-                {flexBoxData.childrenProperties.map(childProp => {
+                {items.map(item => {
                     return (
-                        <div key={childProp.title}>
-                            <h3>{childProp.title}</h3>
-                            {childProp.items.map(item => {
-                                return (
-                                    <ChildPropsDropDown 
-                                        key={item} 
-                                        letter={item} 
-                                        setCurrent={() => this.setState({ currentProp: childProp.camelCaseTitle})}
-                                        handleNumberSelection = {this.updateNumberSelection} 
-                                        bgColor={globalStyle.childPropColors[`${item}`]} 
-                                    />
-                                )
-                            })}
-                            <ChildPropsItemContainer 
-                                {...this.props}
-                                flexBoxDataItems={childProp.items} 
-                                flexShrink={this.state.flexShrink}
-                                flexGrow={this.state.flexGrow}
-                                //flexGrow={(childProp.title === "flex-grow") ? this.state.flexGrow : { a: 1, b: 2, c: 1}} 
-                                
-
-                                />
-                        </div>
+                        <ChildPropsDropDown 
+                            key={item} 
+                            letter={item} 
+                            // setCurrent={() => this.setState({ currentProp: childProp.camelCaseTitle})}
+                            handleNumberSelection = {this.updateNumberSelection} 
+                            bgColor={globalStyle.childPropColors[`${item}`]} 
+                        />
                     )
                 })}
+                <ChildPropsItemContainer 
+                    flexBoxDataItems={items} 
+                    flexShrink={this.state.flexShrink}
+                    flexGrow={this.state.flexGrow}
+                    {...this.props}
+                />
             </div>
-        );
+        )
     }
+    // render() {
+    //     return (
+    //         <div style={{marginBottom: '200px'}}>
+    //             {flexBoxData.childrenProperties.map(childProp => {
+    //                 return (
+    //                     <div key={childProp.title}>
+    //                         <h3>{childProp.title}</h3>
+    //                         {childProp.items.map(item => {
+    //                             return (
+    //                                 <ChildPropsDropDown 
+    //                                     key={item} 
+    //                                     letter={item} 
+    //                                     setCurrent={() => this.setState({ currentProp: childProp.camelCaseTitle})}
+    //                                     handleNumberSelection = {this.updateNumberSelection} 
+    //                                     bgColor={globalStyle.childPropColors[`${item}`]} 
+    //                                 />
+    //                             )
+    //                         })}
+    //                         <ChildPropsItemContainer 
+    //                             flexBoxDataItems={childProp.items} 
+    //                             flexShrink={this.state.flexShrink}
+    //                             flexGrow={this.state.flexGrow}
+    //                             {...this.props}
+
+    //                             />
+    //                     </div>
+    //                 )
+    //             })}
+    //         </div>
+    //     );
+    // }
 }
 
 export default ChildPropsContainer;
